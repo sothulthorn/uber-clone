9f1eecb5de9f02c0306210c50e68c8860de1dbff
sothulthorn <thornsothul@gmail.com>
2025-01-22 22:19:38 +0900

    Connect to Neon database

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -7,6 +7,7 @@ import { Link, router } from "expo-router";
 import OAuth from "@/components/OAuth";
 import { useSignUp } from "@clerk/clerk-expo";
 import { ReactNativeModal } from "react-native-modal";
+import { fetchAPI } from "@/lib/fetch";
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
@@ -52,7 +53,14 @@ const SignUp = () => {
       });
 
       if (signUpAttempt.status === "complete") {
-        // TODO: Create a database user!
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: signUpAttempt.createdUserId,
+          }),
+        });
 
         await setActive({ session: signUpAttempt.createdSessionId });
         setVerification({ ...verification, state: "success" });
